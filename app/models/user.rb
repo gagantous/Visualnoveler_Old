a524016689f5dc0003f0a6833e0422aceee40b2e
wraithseeker <wraithseekers@gmail.com>
2015-11-05 21:17:55 +0800

    add signup with name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
-   has_many :library_entries
+
+  # validates :name, presence: true
+   has_many :library_entries, dependent: :destroy
    has_many :favourites, through: :library_entries, source: :vn
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
      :omniauthable, :omniauth_providers => [:facebook]
-
+  validates :name, presence: true
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
