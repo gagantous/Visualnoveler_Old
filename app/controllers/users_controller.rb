f77e80c518861e33647fed74b4c183043c0f669e
wraithseeker <wraithseekers@gmail.com>
2015-11-15 22:10:24 +0800

    add favourites portion of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
 	@user = User.find(params[:id])
 	@posts = @user.posts.order('created_at DESC')
+  @favourites = @user.favourites
   end
 
   def watch
