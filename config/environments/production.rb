426a2478b8615cf573c0a624c6990fb1147875c7
wraithseeker <wraithseekers@gmail.com>
2015-11-19 16:08:34 +0800

    add standing issues and production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,20 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  config.action_mailer.default_url_options = { host: 'visualnoveler.com' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+    }
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
