35158f6d26bac934605aa5ec24b5352b58c38d3e
wraithseeker <wraithseekers@gmail.com>
2015-11-19 17:48:06 +0800

    params devise

--- config/initializers/DevisePermittedParameters.rb
+++ config/initializers/DevisePermittedParameters.rb
@@ -8,7 +8,9 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:sign_up) << :name 
+	  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email,:bio, :password,
+		      :password_confirmation, :current_password, :poster_image,:poster_image_crop_x, :poster_image_crop_y, :poster_image_crop_w, :poster_image_crop_h) }
   end
 
 end
