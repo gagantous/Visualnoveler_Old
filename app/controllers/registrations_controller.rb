3d0b9ca087f1e3aaa39d0be7bcfd07e4a5e8a07a
wraithseeker <wraithseekers@gmail.com>
2015-11-19 17:41:36 +0800

    registration test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
+ before_action :configure_permitted_parameters
+
+  def update_sanitized_params
+       devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
+    end
   protected
 
     def after_update_path_for(resource)
