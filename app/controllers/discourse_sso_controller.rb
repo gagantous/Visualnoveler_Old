dbc172c843bace24bdccd9e571af07dab6ea54b3
wraithseeker <wraithseekers@gmail.com>
2016-01-09 20:26:17 +0800

    update sso

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -1,3 +1,4 @@
+require 'single_sign_on'
 class DiscourseSsoController < ApplicationController
 	before_action :authenticate_user! # ensures user must login
 
@@ -5,11 +6,11 @@ class DiscourseSsoController < ApplicationController
     secret = "wraith_test"
     sso = SingleSignOn.parse(request.query_string, secret)
     sso.email = current_user.email # from devise
-    sso.name = current_user.full_name # this is a custom method on the User class
+    sso.name = current_user.name # this is a custom method on the User class
     sso.username = current_user.email # from devise
     sso.external_id = current_user.id # from devise
     sso.sso_secret = secret
 
-    redirect_to sso.to_url("http://forums.visualnoveler/session/sso_login")
+    redirect_to sso.to_url("http://forums.visualnoveler.com/session/sso_login")
   end
 end
