dbc52e7943543a86c7cd4665f4eb59334720fe5e
wraithseeker <wraithseekers@gmail.com>
2016-03-13 17:42:57 +0800

    update discourse SSO to synchronize avatars

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -3,14 +3,14 @@ class DiscourseSsoController < ApplicationController
 	before_action :authenticate_user! # ensures user must login
 
   def sso
-    secret = "wraith_test"
+    secret = ENV["DISCOURSE_SECRET_KEY"]
     sso = SingleSignOn.parse(request.query_string, secret)
     sso.email = current_user.email # from devise
-    sso.name = current_user.name # this is a custom method on the User class
-    sso.username = current_user.email # from devise
+    #sso.name = current_user.name # for now we want users to be able to pick a custom name 
+    sso.username = current_user.name # from devise
     sso.external_id = current_user.id # from devise
     sso.sso_secret = secret
-
+    sso.avatar_url = current_user.poster_image.url
     redirect_to sso.to_url("http://forums.visualnoveler.com/session/sso_login")
   end
 end
