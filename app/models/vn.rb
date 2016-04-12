10c01920f3f5aa0d936a65d43f79304ef0741888
wraithseeker <wraithseekers@gmail.com>
2016-04-12 22:00:48 +0800

    remove simple since it's defautl in pg

--- app/models/vn.rb
+++ app/models/vn.rb
@@ -35,7 +35,7 @@ class Vn < ActiveRecord::Base
   	include PgSearch
   	#:associated_against => { genres: [:name]},
   	pg_search_scope :search_by_name, :against => [:name,:alias],:using => {
-                    :tsearch => {:prefix => true,:dictionary => "simple"}
+                    :tsearch => {:prefix => true}
                   }
     scope :created_between, lambda {|start_date, end_date| where("created_at >= ? AND created_at <= ?", start_date, end_date )}
     extend FriendlyId
