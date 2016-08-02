49b904ff12aec4e7445cd7f71579ae939caf7e54
wraithseeker <wraithseekers@gmail.com>
2016-08-02 16:48:53 +0800

    convert coffeescript to javascript

--- app/assets/javascripts/vn_typeahead.js
+++ app/assets/javascripts/vn_typeahead.js
@@ -1,42 +1,33 @@
+$(function() {
+  users = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.whitespace,
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  limit: 6,
+  remote: {url:'/api/vns/search/%QUERY'
+   , wildcard: '%QUERY'
+   }
+   });
+  users.initialize();
+  $('.typeahead').typeahead(null, {
+      name: "mysearch",
+      display: 'name',
+      limit: 6,
+      source: function(query, syncResults, asyncResults) {
+			    $.get('/api/vns/search/' + query, function(data) {
+			      asyncResults(data);
+			    });
+			},
+      templates: {
+         suggestion: Handlebars.compile('<div class="tt-row"><a href="{{vn_url}}"> \
+                                          <ul class="list-inline"><li><div class="col-md-4 removepadding"> \
+                                          <img class="img-responsive" src="{{image_poster.image_poster.thumb.url}}"/> \
+                                          </div><div class="col-md-8"><span class="tt-title">{{trimString name}}</span></div></li></ul></a></div>')
+   		}
+   	});
+});
 
-
-// function initializeTypeahead(typeaheadInputBox, dataSource)
-// {
-//   var vn = new Bloodhound({
-//   datumTokenizer: function(datum) {
-//       return Bloodhound.tokenizers.whitespace(datum.name);
-//   },
-//   queryTokenizer: Bloodhound.tokenizers.whitespace,
-//   prefetch: {
-//         url: dataSource 
-//       },
-//   remote: {url:'/api/vns/search/%QUERY'
-//     , wildcard: '%QUERY'
-//     }
-// });
-
-// $('.typeahead').typeahead({
-//     minLength: 1,
-//     highlight: true
-//   }, {
-//   name: 'vn',
-//   source: vn,
-//   display: 'name',
-//   templates: {
-//       suggestion: Handlebars.compile('<div class="tt-row"><a href="{{vn_url}}">\
-//                                     <ul class="list-inline"><li><div class="col-md-4 removepadding">\
-//                                     <img class="img-responsive" src="{{image_poster.image_poster.thumb.url}}"/>\
-//                                     </div><div class="col-md-8"><strong>{{name}}</strong></div></li></ul></a></div>')
-//   }   
-
-// });
-// }
-
-
-// $(document).ready(function() { 
-//   var source = '/api/vns/all/typeahead';
-//   initializeTypeahead($('.typeahead'), source); 
-//   $(".typeahead").bind('typeahead:select', function (obj, datum,name) {
-//       window.location.href = datum["vn_url"];
-//     });
-// });
+$(function() {
+  $(".typeahead").bind('typeahead:select',function(obj,datum,name) {
+      window.location.href = datum["vn_url"];
+    });
+});
--- app/assets/javascripts/vns.coffee
+++ app/assets/javascripts/vns.coffee
@@ -1,49 +1,8 @@
 jQuery ->
   $('a.fancybox').fancybox({parent: "body"})
 
-# jQuery ->
-#   $('a.vn-tooltip-related').popover();
-
 jQuery ->
   $('.vn-tooltip-related').tooltip();
-
-# $ ->
-#   $(".typeahead").bind "typeahead:select", ->
-#     window.location = "http://localhost:3000/vns";
-
-
-## OLD TYPEAHEAD CODE FOR REMOTE STUFF
-jQuery -> 
-  users = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  limit: 6,
-  remote: {url:'/api/vns/search/%QUERY'
-   , wildcard: '%QUERY'
-   }
-
-    
-   })
-  users.initialize();
-  $('.typeahead').typeahead(null, {
-      name: "mysearch"
-      display: 'name'
-      limit: 6
-      source: users.ttAdapter()
-      templates: {
-         suggestion: Handlebars.compile('<div class="tt-row"><a href="{{vn_url}}">
-                                          <ul class="list-inline"><li><div class="col-md-4 removepadding">
-                                          <img class="img-responsive" src="{{image_poster.image_poster.thumb.url}}"/>
-                                          </div><div class="col-md-8"><span class="tt-title">{{trimString name}}</span></div></li></ul></a></div>')
-      }     
- })
-
-jQuery ->
-  $(".typeahead").bind('typeahead:select',(obj,datum,name) ->
-      window.location.href = datum["vn_url"]
-    )
-
-
 
 jQuery ->
  $(".fancyframe").fancybox
